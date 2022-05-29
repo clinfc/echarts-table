bbdf1e10ab5cb8dd5e2c5dddfbebfa6f4004a57f
clinfc <1962109568@qq.com>
2022-05-29 20:06:01 +0800

    no message

---
+++ src/components/LineChartTable/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div ref="elRef"></div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, type Ref } from 'vue'
+  import { useLineChartTable } from './useLineChartTable'
+
+  export default defineComponent({
+    name: 'EchartsTable',
+    setup(props) {
+      const elRef = ref<any>(null) as Ref<HTMLDivElement>
+
+      const { instance, config, size, eopts } = useLineChartTable(elRef, {
+        size: {
+          width: window.innerWidth - 60,
+          height: window.innerHeight - 100,
+        },
+        colLabel: ['label', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10', '2018-11', '2018-12'],
+        rowLabel: ['第一行', '第二行', '第三行', '第四行', '第五行', '第六行', '第七行'],
+        data: [
+          [1, 3, 8, 0, 7, 4, 6, 10],
+          [60, 72, 10, 39, 40, 33, 50, 69],
+          [100, 300, 800, 0, 700, 400, 600, 1500],
+          [108, 125, 80, 0, 71, 40, 60, 135],
+          [0.6, 0.72, 0.1, 0.39, 0.4, 0.33, 0.5, 0.69],
+          [1, 3, 8, 0, 7, 4, 6, 2],
+          [0.06, 0.072, 0.01, 0.039, 0.04, 0.033, 0, 0.069],
+        ],
+      })
+
+      Reflect.set(window, 'config', config)
+
+      return { elRef }
+    },
+  })
+</script>
--- src/views/EchartsTable.vue
+++ src/views/EchartsTable.vue
@@ -1,53 +1,12 @@
 <template>
-  <EchartsTable v-bind="bindRaw" />
+  <EchartsTable />
 </template>
 
 <script lang="ts">
-  import { defineComponent, reactive } from 'vue'
-  import EchartsTable from '../components/EchartsTable.vue'
+  import { defineComponent } from 'vue'
+  import EchartsTable from '../components/LineChartTable/index.vue'
 
   export default defineComponent({
     components: { EchartsTable },
-    setup() {
-      const bindRaw = reactive({
-        data: [
-          {
-            label: '第一行',
-            '2018-06': 4,
-            '2018-07': 4,
-            '2018-08': 5,
-            '2018-09': 4,
-            '2018-10': 3,
-            '2018-11': 1,
-            '2018-12': 2,
-          },
-          {
-            label: '第二行',
-            '2018-06': 1,
-            '2018-07': 4,
-            '2018-08': 3,
-            '2018-09': 4,
-            '2018-10': 5,
-            '2018-11': 1,
-            '2018-12': 2,
-          },
-          {
-            label: '第三行',
-            '2018-06': 4,
-            '2018-07': 1,
-            '2018-08': 5,
-            '2018-09': 2,
-            '2018-10': 3,
-            '2018-11': 4,
-            '2018-12': 2,
-          },
-        ],
-        labelKeys: ['label', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10', '2018-11', '2018-12'],
-        width: window.innerWidth - 40,
-        height: window.innerHeight - 40,
-      })
-
-      return { bindRaw }
-    },
   })
 </script>
